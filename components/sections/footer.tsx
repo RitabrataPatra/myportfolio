8489b37c184a7df068c498a31d7a2ce14260c283
Ritabrata Patra <teamritabrata@gmail.com>
2025-01-05 20:25:05 +0530

    made some  dark mode theme fixes

--- components/sections/footer.tsx
+++ components/sections/footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='border-t border-neutral-800 bg-neutral-950'>
+    <div className='border-t dark:border-neutral-800 dark:bg-neutral-950'>
         <div className='flex items-center justify-center py-4'>
             <p>© 2025 Ritabrata Patra. All rights reserved.</p>
         </div>
